ebc546f748e8648422d606c58effabfef1f742ab
gatwich <gatwich@gmail.com>
2018-09-19 09:15:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
+var RequestBody;
+
+
+
+var JsonRequest = require('request');
+request('https://www.albertahealthservices.ca/webapps/VoiceAppDev/ahs-alexa-news-dev/audioPOC/response-json.json', function (error, response, body) {
+  requestbody = body;
+});
+
+
+
 restService.use(
   bodyParser.urlencoded({
       extended: true
@@ -22,46 +33,8 @@ restService.post("/TipOfDay", function(req, res) {
 
   return res.json({
 	  
-"speech": "",
-      "messages": [
-        {
-          "type": 4,
-          "payload": {
-            "google": {
-              "richResponse": {
-                "items": [
-                  {
-                    "simpleResponse": {
-                      "textToSpeech": "From Alberta health Services tip of the day"
-                    }
-                  },
-                  {
-                    "mediaResponse": {
-                      "mediaType": "AUDIO",
-                      "mediaObjects": [
-                        {
-                          "name": "Alberta Health Services Wellness Tips",
-                          "description": "playing",
-                          "largeImage": {
-                            "url": "https://capsconnections.ualberta.ca/caplet/Media/Logo?orgId=79&size=Large",
-                            "accessibilityText": "..."
-                          },
-                          "contentUrl": "https://ahamms01.https.internapcdn.net/ahamms01/Content/AHS_Website/alexa/alexa-fs-less-than-zen.mp3"
-                        }
-                      ]
-                    }
-                  }
-                ],
-                "suggestions": [
-                  {
-                    "title": "chips"
-                  }
-                ]
-              }
-            }
-          }
-        }
-      ]
+	RequestBody
+	  
   });
 });
 
